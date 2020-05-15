c50c313191fb2fe23f69ba64728785b995de5292
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-05-15 10:57:57 -0600

    update project to pass test

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,15 @@
 class Project <ApplicationRecord
   validates_presence_of :name, :material
   belongs_to :challenge
+  has_many :contestant_projects
+  has_many :contestants, through: :contestant_projects
+  def total_contestants
+    contestants.length
+  end
+  def average_year_of_experience
+    years = contestants.map do |contestant|
+      contestant.years_of_experience
+    end
+    years.inject{ |sum, n| sum + n }.to_f / years.size
+  end
 end
